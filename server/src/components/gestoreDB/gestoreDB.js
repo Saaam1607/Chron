24a5606df673c9f446006c8554b918218fe01bd4
Haroon Chaudhry <h.chaudhrymohammad@studenti.unitn.it>
2023-05-15 22:51:48 +0200

    implementata la funzione salvaSessione

--- server/src/components/gestoreDB/gestoreDB.js
+++ server/src/components/gestoreDB/gestoreDB.js
@@ -55,8 +55,39 @@ class GestoreDB {
                     reject(error); // Reject with the error if there was an error
                 });
           });
-        }
+    }
 
-    }   
+    static salvaSessione(sessione, ID_utente) {
+        return new Promise(async (resolve, reject) => {
+            try {
+            const nuovaSessione = new SessioneModel({
+                ID_utente: ID_utente,
+                data: new Date(Date.UTC(sessione.data.anno, sessione.data.mese - 1, sessione.data.giorno)),
+                minuti: sessione.tempo.ore * 60 + sessione.tempo.minuti,
+            });
+        
+            await nuovaSessione.save();
+            resolve();
+            } catch (error) {
+            console.error(`Errore durante il salvataggio della sessione per l'utente ${ID_utente}: ${error}`);
+        
+            // Gestione degli errori specifici
+            if (error.name === 'ValidationError') {
+                // Validazione fallita
+                reject({ errorCode: 1001, message: 'Errore di validazione dei dati' });
+            } else if (error.name === 'MongoError' && error.code === 11000) {
+                // Chiave duplicata
+                reject({ errorCode: 1002, message: 'Duplicazione della chiave unica' });
+            } else {
+                // Altro errore generico
+                reject({ errorCode: 1000, message: 'Errore generico durante il salvataggio della sessione' });
+            }
+            }
+        });
+    }
+
+}
+    
+    
 
 module.exports = GestoreDB;
