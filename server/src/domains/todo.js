8a0ab21ba290ae4c28b7abe7efc3d26b33c12911
Haroon Chaudhry <h.chaudhrymohammad@studenti.unitn.it>
2023-05-19 17:19:53 +0200

    API /new creazione task

--- server/src/domains/todo.js
+++ server/src/domains/todo.js
@@ -10,16 +10,41 @@ router.get('/', async (req, res) => {
 	console.log("GET /todos");
   
 	try {
-	  const todos = await listaTask.leggiTasks();
-      
-	  if(todos.length == 0){
-		res.status(204).json({ success: true, message: "Non ci sono task da mostrare" });
-	  }else{
-		res.status(200).json({ success: true, tasks: todos });
-	  }
+        const todos = await listaTask.leggiTasks();
+
+        if(todos.length == 0){
+            res.status(204).json({ success: true, message: "Non ci sono task da mostrare" });
+        }else{
+            res.status(200).json({ success: true, tasks: todos });
+        }
 	} catch (error) {
-	  console.error(`Errore durante la lettura delle tasks: ${error.message}`);
-	  res.status(500).json({ success: false, message: `L'operazione di lettura delle task non è andata a buon fine. ${error.message}` });
+        console.error(`Errore durante la lettura delle tasks: ${error.message}`);
+        res.status(500).json({ success: false, message: `L'operazione di lettura delle task non è andata a buon fine. ${error.message}` });
+	}
+});
+
+router.post('/new', async (req, res) => {
+	console.log("POST /todo/new");
+	const { nome, dataScadenza } = req.body;
+  
+	if (!nome ) {
+        res.status(400).json({ success: false, message: "Nome mancante" }); 
+        return;
+	}
+  
+	const nuovaTask = new Task(listaTask.ID_utente, nome, dataScadenza);
+  
+	try {
+	  	const task  = await nuovaTask.crea();
+		nuovaTask._id = task._id;
+		nuovaTask.contrassegna = task.contrassegna;
+		nuovaTask.gruppoID = task.gruppoID;
+		listaTask.tasks.push(nuovaTask);
+
+		res.status(201).json({success: true, task:task}); 
+	} catch (error) {
+        console.error(`Errore durante la creazione della task: ${error.message}`);
+        res.status(500).json({ success: false, message: `Si è verificato un errore durante la creazione della task. Errore: ${error.message}` });
 	}
   });
 
