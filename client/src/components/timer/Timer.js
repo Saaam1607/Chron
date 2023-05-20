dbe14db0a6cc4dce3b59f05e357e18764f3309f2
GiulioUnitn <giulio.zamberlan@studenti.unitn.it>
2023-05-20 12:34:32 +0200

    update

--- client/src/components/timer/Timer.js
+++ client/src/components/timer/Timer.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react';
 import './Timer.css';
-import SessionForm from './SessionForm';
+//import SessionForm from './SessionForm';
 import TimerSettings from "./TimerSettings"
 import { handleAlert } from '../alert/Alert';
 //const tokenManager = require('../tokenManager/cookieManager');
-const tokenManager = require('../tokenManager/cookieManager');
 
 
 export default function Timer(){
