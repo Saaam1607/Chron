2cec0b43ec555541decc45126ef63ce0d1e8a348
Samuel Casagrande Cecchin <s.casagrandececchin@studenti.unitn.it>
2023-05-30 12:47:28 +0200

    api and UI

--- client/src/components/profilo/DatiProfilo.js
+++ client/src/components/profilo/DatiProfilo.js
@@ -1,21 +1,61 @@
+import { useState, useEffect } from 'react';
+
 import CookieManager from'../tokenManager/cookieManager';
+
+
 
 export default function Profilo({setAuthenticated}){
 
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+
+
+
+    useEffect(() => {
+        // ricerca dei dati utente
+        fetch("/api/v1/profilo/data", {
+            method: "GET",
+            headers: CookieManager.generateHeader(),
+        })
+        .then(response => {
+
+            if (response.status === 200) {
+                return response.json();
+            } else if (response.status === 400){
+                return response.json().then(errorData => {
+                    throw new Error(errorData.message);
+                });
+            } else if (response.status === 500){
+                return response.json().then(errorData => {
+                    throw new Error(errorData.message);
+                });
+            }
+        })
+                .then(data => {
+                    // setta i dati utente
+                    setUsername(data.username);
+                    setEmail(data.email);
+                })
+                    .catch(error => {
+                        alert(error.message);
+                    })
+            
+    }, [])
+
+
     return (
       <div>
-        {/* <h1>Profilo</h1>
             <div>
                 <h2> username: </h2>
-                <p>bla bla bla</p>
+                <p>{username}</p>
             </div>
             <div>
                 <h2>email: </h2>
-                <p>bla bla bla</p>
+                <p>{email}</p>
             </div>
             <div>
                 <button>Logout</button>
-            </div> */}
+            </div>
             <button
                 onClick={() => {
                     CookieManager.deleteAuthToken();
