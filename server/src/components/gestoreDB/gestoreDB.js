396fee840d9ae71062918999567a0607c83595b9
Haroon Chaudhry <h.chaudhrymohammad@studenti.unitn.it>
2023-06-04 19:52:19 +0200

    aggiornaPassword by ID

--- server/src/components/gestoreDB/gestoreDB.js
+++ server/src/components/gestoreDB/gestoreDB.js
@@ -329,12 +329,11 @@ class GestoreDB {
         });
     }
 
-    static getDatafromEmail(email) {
+    static getDataFromEmail(email) {
         return new Promise((resolve, reject) => {
             Credenziali.findOne({ email: email })
                 .then((result) => {
                     if (result) {
-                        resolve(result._id); // Resolve with true if the entry exists
                         resolve({success: true, _id: result._id, username: result.username, email: result.email}); // Resolve with true if the entry exists
                     } else {
                         resolve(null); // no result
@@ -345,6 +344,23 @@ class GestoreDB {
                 });
         });
     }
+
+    static aggiornaPassword(ID_utente, nuova_password) {
+        return new Promise((resolve, reject) => {
+          Credenziali.updateOne({ _id: new mongoose.Types.ObjectId(ID_utente) }, { password: nuova_password })
+            .then((result) => {
+              if (result) {
+                resolve({ success: true, _id: result._id });
+              } else {
+                reject({ message: "Utente non trovato" });
+              }
+            })
+            .catch((error) => {
+              reject({ message: "Errore nella ricerca dell'utente", error });
+            });
+        });
+    }
+
 
 }
     
