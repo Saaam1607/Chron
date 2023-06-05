309a55710a88bf0a2e87e6d3083d9f61228e6c60
Samuel Casagrande Cecchin <s.casagrandececchin@studenti.unitn.it>
2023-06-05 19:56:54 +0200

    icon addition

--- client/src/components/saleStudio/SalaStudio.js
+++ client/src/components/saleStudio/SalaStudio.js
@@ -9,18 +9,24 @@ export default function SalaStudio({data}){
 
 
     return (
-
         <div>
-
             <div className='salaStudio-div'>
-                <h4 className='salaStudio-nome'>{data.name}</h4>
+
+                <div className='salaStudio-title'>
+                    <span className="icona-salaStudio">
+                        <i
+                            className="bi bi-buildings"
+                            title="SALA STUDIO"
+                        ></i>
+                    </span>
+                    <h4 className='salaStudio-nome'>{data.name}</h4>
+                </div>
+
                 <p className='salaStudio-indirizzo'>{data.address}</p>
                 <p className='salaStudio-restrizioni'>{data.restrictions}</p>
                 <p className='salaStudio-rating'>{data.rating}</p>
                 <Rating rating={data.rating} />
             </div> 
-
         </div> 
-
     );
 }
