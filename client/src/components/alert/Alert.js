4cf83162334e18cdde160ee135c52738cba186a6
Samuel Casagrande Cecchin <s.casagrandececchin@studenti.unitn.it>
2023-06-06 11:43:19 +0200

    alert modifications end

--- client/src/components/alert/Alert.js
+++ client/src/components/alert/Alert.js
@@ -1,8 +1,9 @@
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
-import './Alert.css';
 
 const MySwal = withReactContent(Swal)
+
+
 
 export function handleAlert (message, sound, icon, timer) {
 
@@ -10,20 +11,19 @@ export function handleAlert (message, sound, icon, timer) {
         icon: icon,
         text: message,
         confirmButtonText: 'OK',
-        confirmButtonText: message,
         didOpen: () => {
-          if (sound) {
-            var audio = new Audio('Notification.mp3');
-            audio.play();
-          }
+            if (sound) {
+                var audio = new Audio('Notification.mp3');
+                audio.play();
+            }
         }
-      };
+    };
     
-      if (timer) {
-        configurazioneAlert.timer = timer;
-        configurazioneAlert.timerProgressBar = true;
-        configurazioneAlert.showConfirmButton = false;
-      }
+    if (timer) {
+            configurazioneAlert.timer = timer;
+            configurazioneAlert.timerProgressBar = true;
+            configurazioneAlert.showConfirmButton = false;
+    }
 
     MySwal.fire(configurazioneAlert);
 }
