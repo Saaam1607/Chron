1413a59859c90ce9f135aa206edf3a041a54ec64
Samuel Casagrande Cecchin <s.casagrandececchin@studenti.unitn.it>
2023-06-06 13:32:10 +0200

    home added

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { format } from "date-fns";
+import { it } from "date-fns/locale";
+import "./Home.css";
+
+
 
 function Home() {
+
+    const currentDate = new Date();
+    const formattedDate = format(currentDate, "EEEE d MMMM", { locale: it });
+
+    const [currentTime, setCurrentTime] = useState(new Date());
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+          setCurrentTime(new Date());
+        }, 1000);
+    
+        return () => {
+          clearInterval(timer);
+        };
+      }, []);
+
+      const formattedTime = currentTime.toLocaleTimeString();
+
+
     return (
-        <div>
-            <h1>Welcome to Chron</h1>
+        <div className="home-container">
+
+
+            <div className="introduction-div">
+                <h1 className="welcome-message">Benevnuto su Chron!</h1>
+            
+                <div className="data-e-ora">
+                    <p className="data">
+                        {formattedDate}
+                    </p>
+                    <p className="ora">
+                        {formattedTime}
+                    </p>
+                </div>
+
+            </div>
+
         </div>
     )
 }
