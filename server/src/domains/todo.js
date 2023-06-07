be66d6b1d110ae1e31a38a9c80fed96789eba421
Haroon Chaudhry <h.chaudhrymohammad@studenti.unitn.it>
2023-06-07 10:19:09 +0200

    Refactored POST /todo/ endpoint

--- server/src/domains/todo.js
+++ server/src/domains/todo.js
@@ -32,38 +32,23 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
 	console.log("POST /todo/");
-    const { _id, ID_utente, nome, dataScadenza, ID_gruppo } = req.body;
+	const { nome, dataScadenza } = req.body;
   
 	if (!nome) {
         res.status(400).json({ success: false, message: "Nome mancante" }); 
         return;
 	}
-
-    let nuovaTask;
     
-    // Se non è presente l'id, la task è personale
-    if(_id == null){
-        nuovaTask = new Task(req.id, nome, dataScadenza);
-    }else{
-        nuovaTask = new Task(ID_utente, nome, dataScadenza);
-        nuovaTask._id = _id;
-        nuovaTask.ID_gruppo = ID_gruppo;
-    }
-  
 	try {
-	  	const task  = await nuovaTask.crea();
 
+        const nuovaTask = new Task(req.id, nome, dataScadenza);
+
+	  	const task  = await nuovaTask.crea();
 		res.status(201).json({success: true, task:task}); 
 
 	} catch (error) {
-        if(error.message.includes("esistente")) {
-            res.status(409).json({ success: false, message: `Task già accettata` });
-        }
-        else {
-            console.error(`L'operazione di creazione della task non è andata a buon fine. ${error.message}`);
-            res.status(500).json({ success: false, message: `L'operazione di creazione della task non è andata a buon fine. ${error.message}` });
-        }
-
+        console.error(`L'operazione di creazione della task non è andata a buon fine. ${error.message}`);
+        res.status(500).json({ success: false, message: `L'operazione di creazione della task non è andata a buon fine. ${error.message}` });
 	}
 });
 
