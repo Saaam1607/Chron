f2e7a53a0c24ff5695b8068dfa106eb626c97155
Haroon Chaudhry <h.chaudhrymohammad@studenti.unitn.it>
2023-06-07 21:17:40 +0200

    fetching /autenticazioneEsterna

--- client/src/components/profilo/AutenticazioneEsterna.js
+++ client/src/components/profilo/AutenticazioneEsterna.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState,useContext } from "react";
 import { GoogleLogin } from '@react-oauth/google';
-
+import CookieManager from'../tokenManager/cookieManager';
+import { handleAlert } from "../alert/Alert";
 
 
 
@@ -8,7 +9,27 @@ export default function SignUp() {
     
     const continueWithGoogle =(credentialResponse)=>{
         console.log(credentialResponse);
-
+        console.log(credentialResponse.credential)
+        fetch("api/v1/profilo/autenticazioneEsterna", {
+        method: "post",
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            gToken: credentialResponse.credential,
+            clientId: credentialResponse.clientId
+        })
+        }).then(res => res.json())
+        .then(data => {
+            if (data.success === false) {
+                handleAlert(data.message, false, "error")
+            } else {
+                console.log(data)
+                CookieManager.setAuthToken(data.token);
+            }
+            console.log(data)
+        })
     }
 
     return (
