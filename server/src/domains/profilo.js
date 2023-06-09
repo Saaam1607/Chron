14280e707959c1c076fd69f718209f77fc7987eb
Haroon Chaudhry <h.chaudhrymohammad@studenti.unitn.it>
2023-06-09 10:59:31 +0200

    standardization of return messages for profilo.js

--- server/src/domains/profilo.js
+++ server/src/domains/profilo.js
@@ -19,23 +19,23 @@ router.post("/autenticazione", bodyParser.json(), (req, res) => {
               return GestoreDB.getIDfromEmail(req.body.email)
                   .then((esito) => {
                       return res.status(200).json({
-                          success: "true",
+                          success: true,
                           token: jwt.sign({ id: esito }, process.env.ACCESS_TOKEN_SECRET),
                       });
                   });
           } else{
-              res.status(401).json({success: "false", message: "Autenticazione non riuscita, credenziali non corrispondenti o valide"})
+              res.status(401).json({success: false, message: `Autenticazione non riuscita, credenziali non corrispondenti o valide`})
           }
       })
       .catch((error) => {
-          res.status(500).json({success: "false", message: "Errore durante l'autenticazione"})
+          res.status(500).json({success: false, message: `L'operazione di richiesta di autenticazione non è andata a buon fine. ${error.message}`})
       });
 })
 
 router.post("/nuova-autenticazione", bodyParser.json(), async (req, res) => {
   try {
     if (await GestoreDB.controllaEsistenzaEmail(req.body.email)) {
-      return res.status(409).json({ success: false, message: "Errore, email già utilizzata" });
+      return res.status(409).json({ success: false, message: `Errore, email già utilizzata` });
     }
 
     if (
@@ -49,7 +49,7 @@ router.post("/nuova-autenticazione", bodyParser.json(), async (req, res) => {
       // controlli sul formato dell'username
       req.body.username.length < 4
     ) {
-      return res.status(401).json({ success: false, message: "Errore, credenziali non valide" });
+      return res.status(401).json({ success: false, message: `Le credenziali fornire non sono valide!` });
     }
 
     const token = jwt.sign({ email: req.body.email, password: req.body.password, username: req.body.username }, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
@@ -60,10 +60,10 @@ router.post("/nuova-autenticazione", bodyParser.json(), async (req, res) => {
 
     await GestoreDB.salvaToken(token);
 
-    res.status(200).json({ success: true, message: "Email di verifica inviata con successo!" });
+    res.status(200).json({ success: true, message: `Email di Conferma Account inviata con successo!` });
   } catch (error) {
-    console.error("Errore durante la registrazione:", error);
-    res.status(500).json({ success: false, message: "Errore durante la registrazione e invio email" });
+    console.error( `L'operazione di richiesta di registrazione non è andata a buon fine. ${error.message}`);
+    res.status(500).json({ success: false, message:  `L'operazione di richiesta di registrazione non è andata a buon fine. ${error.message}` });
   }
 });
 
@@ -71,12 +71,12 @@ router.post("/verifica-registrazione", bodyParser.json(), async (req, res) => {
   const { token } = req.body;
 
   if(!await GestoreDB.checkIfTokenExist(token)) {
-    return res.status(409).json({ success: false, message: "Account già esistente!" });
+    return res.status(409).json({ success: false, message: `Account già esistente!` });
   }
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async(error, decodedToken) => {
     if (error) {
-      return res.status(401).json({ success: "false", message: "Token di verifica non valido o scaduto." });
+      return res.status(401).json({ success: false, message: `Token non valido o scaduto! ${error.message} ` });
     }
 
     const { email, password, username } = decodedToken;
@@ -86,10 +86,10 @@ router.post("/verifica-registrazione", bodyParser.json(), async (req, res) => {
 
       await GestoreDB.deleteToken(token);
 
-      res.status(201).json({ success: true, message: "Account registrato con successo!" });
+      res.status(201).json({ success: true, message: `Account registrato con successo!` });
 
     } catch (error) {
-      res.status(500).json({ success: false, message: `Errore durante la registrazione: ${error}` });
+      res.status(500).json({ success: false, message: `L'operazione di registrazione non è andata a buon fine. ${error.message}` });
     }
   });
 });
@@ -100,12 +100,12 @@ router.post("/richiesta-nuova-password", (req, res) => {
   const { email } = req.body;
 
   if (email == undefined) {
-    return res.status(400).json({ success: "false", message: "Errore, email mancante!" });
+    return res.status(400).json({ success: false, message: `Il parametro "email" mancante!` });
   } else {
     GestoreDB.getDataFromEmail(email)
       .then((result) => {
         if (result == null) {
-          return res.status(404).json({ success: "false", message: "Utente non trovato!" });
+          return res.status(404).json({ success: false, message: `Utente non trovato con l'email fornita!` });
         }
 
         // recupero password solo se l'account è stato creato con la registrazione interna di CHRON        
@@ -113,7 +113,7 @@ router.post("/richiesta-nuova-password", (req, res) => {
 
         GestoreDB.login(email, password).then((login) => {
           if(login) {
-            return res.status(409).json({ success: false, message: `Non puoi recuperare la password di un account creato con  la registrazione esterna. Per autenticarti utilizza direttamente l'apposito bottone` });
+            return res.status(409).json({ success: false, message: `Non puoi recuperare la password di un account creato con  la registrazione esterna. Per autenticarti utilizza direttamente l'apposito bottone "accedi con google".` });
           } else {
             
             // Procedi con il recupero della password
@@ -123,14 +123,14 @@ router.post("/richiesta-nuova-password", (req, res) => {
 
             GestoreEmail.inviaEmailRecuperoPwd([email], "Recupero password", recoveryLink);
 
-            res.status(200).json({ success: "true", message: "Email inviata con successo!" });
+            res.status(200).json({ success: true, message: `Email inviata con successo!` });
           }
         });
 
 
       })
       .catch((error) => {
-        res.status(500).json({ success: "false",  message: `Errore durante il forgot-password: ${error}` });
+        res.status(500).json({ success: false,  message: `L'operazione di richiesta nuova passsword non è andato a buon fine. ${error.message}` });
       });
   }
 });
@@ -142,7 +142,7 @@ router.post("/richiesta-reset-password", (req, res) => {
   // Verifica e decodifica del token di recupero password
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decodedToken) => {
     if (error) {
-      return res.status(401).json({ success: "false", message: "Invalid or expired token:" + error.message });
+      return res.status(401).json({ success: false, message: `Token non valido o scaduto! ${error.message}`});
     }
 
     const { id } = decodedToken;
@@ -150,10 +150,10 @@ router.post("/richiesta-reset-password", (req, res) => {
     // Aggiornamento della password nel database
     GestoreDB.aggiornaPassword(id, password)
       .then(() => {
-        res.status(200).json({ success: "true", message: "Password aggiornata con successo!" });
+        res.status(200).json({ success: true, message: `Password aggiornata con successo!` });
       })
       .catch((error) => {
-        res.status(500).json({ success: "false",  message: `Errore durante il reset-password: ${error}` });
+        res.status(500).json({ success: false,  message: `L'operazione di ripristino della password non è andato a buon fine. ${error.message}` });
       });
     });
 });
@@ -163,7 +163,7 @@ router.post("/autenticazioneEsterna", async (req, res) => {
   const { gToken, clientId } = req.body;
 
   if (!gToken || !clientId) {
-    return res.status(400).json({ success: false, message: "Errore, token o clientId mancanti!" });
+    return res.status(400).json({ success: false, message: `I parametri "gToken" o "clientId" mancanti!` });
   }
 
   try {
@@ -180,13 +180,13 @@ router.post("/autenticazioneEsterna", async (req, res) => {
         const login = await GestoreDB.login(email, password);
 
         if(!login) {
-          return res.status(409).json({ success: false, message: `Questa email è già stata utilizza per creare l'account con  la registrazione interna di CHRON. Se hai dimenicato la password pui recuperarla tramite l'apposito form` });
+          return res.status(409).json({ success: false, message: `Questa email è già stata utilizza per creare l'account con  la registrazione interna di CHRON. Se hai dimenicato la password puoi recuperarla tramite l'apposito form` });
         }
 
         const _id = await GestoreDB.getIDfromEmail(email);
         const token = jwt.sign({ id: _id }, process.env.ACCESS_TOKEN_SECRET);
 
-        return res.status(200).json({ success: true, message: "Login effettuato con successo!", token });
+        return res.status(200).json({ success: true, message: `L'autenticazione effettuata con successo!`, token });
 
       } else {
         // registrazione (con notifica email) e login
@@ -196,14 +196,14 @@ router.post("/autenticazioneEsterna", async (req, res) => {
 
         GestoreEmail.inviaEmailBenvenuto([email], "ACCOUNT CREATO");
 
-        return res.status(201).json({ success: true, message: "Utente registrato e Login effettuato con successo!", token });
+        return res.status(201).json({ success: true, message: `Utente registrato e l'autenticazione effettuata con successo!`, token });
 
       }
     } else {
-      return res.status(401).json({ success: false, message: "Email non verificata!" });
+      return res.status(401).json({ success: false, message: `L'operazione di autenticazione esterna non è andata a buon fine. L'email non è verificata!` });
     }
   } catch (error) {
-    return res.status(500).json({ success: false, message: `L'operazione di autenticazione esterna non è andata a buon fine. ${error}` });
+    return res.status(500).json({ success: false, message: `L'operazione di autenticazione esterna non è andata a buon fine. ${error.message}` });
   }
 });
 
