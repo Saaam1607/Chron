3e9c051bcf0ab385aa0b8d61fa0b4525ced3a6ba
Haroon Chaudhry <h.chaudhrymohammad@studenti.unitn.it>
2023-06-10 03:48:51 +0200

    coverage 74 percent on profilo.js

--- server/test/profilo_endpoints.test.js
+++ server/test/profilo_endpoints.test.js
@@ -36,6 +36,13 @@ describe('API /api/v1/profilo endpoints', () => {
         email: 'test@example.com',
         password: 'Password123',
     };
+
+    const externalUser = {
+        _id: new mongoose.Types.ObjectId(),
+        username: 'test2',
+        email: 'test1@example.com',
+        password: 'test1@example.com'+process.env.CLIENT_ID,
+    };
     
     let token = 'Bearer ' + jwt.sign({ id: '64765870316275628c4870b9' }, process.env.ACCESS_TOKEN_SECRET);
     const tokenRegistrazione =  jwt.sign({ emai: user.email, password: user.password, username:user.username}, process.env.ACCESS_TOKEN_SECRET, { expiresIn: "1h" });
@@ -201,6 +208,121 @@ describe('API /api/v1/profilo endpoints', () => {
 
     });
 
+    test('POST /api/v1/profilo/richiesta-nuova-password should respond with status 400 if email is missing', async () => {
+        const response = await request(app)
+          .post(`${api_url}/richiesta-nuova-password`)
+          .send({});
+      
+        expect(response.status).toBe(400);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe('Il parametro "email" mancante!');
+    });
+
+    test('POST /api/v1/profilo/richiesta-nuova-password should respond with status 404 if user is not found with the provided email', async () => {
+        // Mock the function to return null, indicating user not found
+        //jest.spyOn(GestoreDB, 'getDataFromEmail').mockResolvedValueOnce(null);
+      
+        const response = await request(app)
+          .post(`${api_url}/richiesta-nuova-password`)
+          .send({ email: 'nonexistent@example.com' });
+      
+        expect(response.status).toBe(404);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe('Utente non trovato con l\'email fornita!');
+    });
+
+    test('POST /api/v1/profilo/richiesta-nuova-password should respond with status 409 if account was created with external registration', async () => {
+        // Mock the function to return a successful login, indicating an externally registered account
+        //jest.spyOn(GestoreDB, 'login').mockResolvedValueOnce(true);
+        
+        Credenziali.create(externalUser);
+
+        const response = await request(app)
+          .post(`${api_url}/richiesta-nuova-password`)
+          .send({ email: externalUser.email });
+      
+        expect(response.status).toBe(409);
+        expect(response.body.success).toBe(false);
+    });
+
+    test('POST /api/v1/profilo/richiesta-nuova-password should respond with status 200 and success message if password reset email send successful', async () => {
+        
+        await Credenziali.deleteMany({});
+        await Credenziali.create(user);
+
+        const response = await request(app)
+          .post(`${api_url}/richiesta-nuova-password`)
+          .send({ email: user.email });
+      
+        expect(response.status).toBe(200);
+        expect(response.body.success).toBe(true);
+
+    });
+
+    test('POST /api/v1/profilo/richiesta-nuova-password should respond with status 500 if an error occurs', async () => {
+        jest.spyOn(GestoreDB, 'getDataFromEmail').mockRejectedValueOnce(new Error('Errore'));
+        
+        const response = await request(app)
+          .post(`${api_url}/richiesta-nuova-password`)
+          .send({ email: user.email });
+      
+        expect(response.status).toBe(500);
+        expect(response.body.success).toBe(false);
+
+    });
+
+    test('POST /api/v1/profilo/richiesta-reset-password should respond with status 400 if token or password is missing', async () => {
+        const response = await request(app)
+          .post(`${api_url}/richiesta-reset-password`)
+          .send({ token: 'valid-token' });
+      
+        expect(response.status).toBe(400);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe('I parametri "token" o "password" sono mancanti!');
+    });
+
+
+    test('POST /api/v1/profilo/richiesta-reset-password should respond with status 401 if token is invalid or expired', async () => {
+        // Mock a JWT token verification error
+        jest.spyOn(jwt, 'verify').mockImplementationOnce((token, secret, callback) => {
+          const error = new Error('Invalid or expired token');
+          callback(error);
+        });
+      
+        const response = await request(app)
+          .post(`${api_url}/richiesta-reset-password`)
+          .send({ token: 'invalid-token', password: 'newPassword123' });
+      
+        expect(response.status).toBe(401);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe('Token non valido o scaduto! Invalid or expired token');
+    });
+
+    test('POST /api/v1/profilo/richiesta-reset-password should respond with status 200 and success message if password update is successful', async () => {
+        // Mock the password update function to resolve successfully
+        //jest.spyOn(GestoreDB, 'aggiornaPassword').mockResolvedValueOnce();
+      
+        const response = await request(app)
+          .post(`${api_url}/richiesta-reset-password`)
+          .send({ token: tokenRegistrazione, password: 'newPassword123' });
+      
+        expect(response.status).toBe(200);
+        expect(response.body.success).toBe(true);
+        expect(response.body.message).toBe('Password aggiornata con successo!');
+    });
+    
+    test('POST /api/v1/profilo/richiesta-reset-password should respond with status 500 if an error occurs during password update', async () => {
+        // Mock the password update function to reject with an error
+        jest.spyOn(GestoreDB, 'aggiornaPassword').mockRejectedValueOnce(new Error('Password update error'));
+      
+        const response = await request(app)
+          .post(`${api_url}/richiesta-reset-password`)
+          .send({ token: tokenRegistrazione, password: 'newPassword123' });      
+          
+        expect(response.status).toBe(500);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe('L\'operazione di ripristino della password non è andato a buon fine. Password update error');
+    });
 
 });
 
