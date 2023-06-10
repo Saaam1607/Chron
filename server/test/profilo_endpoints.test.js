b7c78c23c348135b368d5796afc363adc2c33907
Haroon Chaudhry <h.chaudhrymohammad@studenti.unitn.it>
2023-06-10 02:04:05 +0200

    34 percent coverage of profilo.js

--- server/test/profilo_endpoints.test.js
+++ server/test/profilo_endpoints.test.js
@@ -9,7 +9,7 @@ let api_url = '/api/v1/profilo';
 
 beforeAll(async () => {
     jest.setTimeout(8000);
-    await mongoose.connect(process.env.TEST_DB_URI, {
+    await mongoose.connect(process.env.MONGODB_URI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
@@ -40,12 +40,14 @@ describe('API /api/v1/profilo endpoints', () => {
         //await Credenziali.deleteMany({});
     });
 
+    const user = {
+        _id: new mongoose.Types.ObjectId(),
+        username: 'test',
+        email: 'test@example.com',
+        password: 'Password123',
+    };
+
     test('POST /api/v1/profilo/autenticazione should respond with status 200 and a token if authentication is successful', async () => {
-        const user = {
-            _id: new mongoose.Types.ObjectId(),
-            email: 'test@example.com',
-            password: 'password',
-        };
 
         await Credenziali.create(user);
 
@@ -58,6 +60,78 @@ describe('API /api/v1/profilo endpoints', () => {
         expect(response.body.token).toBeDefined();
     });
 
+
+    test('POST /api/v1/profilo/autenticazione should respond with status 401 if authentication fails', async () => {
+
+        const response = await request(app)
+        .post(`${api_url}/autenticazione`)
+        .send({ email: 'test@example.com', password: 'wrongpassword' });
+
+        expect(response.status).toBe(401);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe('Autenticazione non riuscita, credenziali non corrispondenti o valide');
+    });
+
+    
+    test('POST /api/v1/profilo/autenticazione should respond with status 500 if an error occurs', async () => {
+        const loginMock = jest.spyOn(Credenziali, 'findOne').mockImplementationOnce(() => {
+            throw new Error('Errore generato dal mock');
+        });
+
+        const response = await request(app)
+        .post(`${api_url}/autenticazione`)
+        .send(user);
+
+        expect(response.status).toBe(500);
+        expect(response.body.success).toBe(false);
+
+        loginMock.mockRestore();
+    });
+
+    test('POST /api/v1/profilo/nuova-autenticazione should respond with status 409 if email already exists', async () => {
+
+        const response = await request(app)
+        .post(`${api_url}/nuova-autenticazione`)
+        .send(user);
+
+        expect(response.status).toBe(409);
+        expect(response.body.success).toBe(false);
+        expect(response.body.message).toBe('Errore, email già utilizzata');
+    });
+
+    test('POST /api/v1/profilo/nuova-autenticazione should respond with status 201 and a token if registration is successful', async () => {
+        //deleting all users
+        await Credenziali.deleteMany({});
+
+        const response = await request(app)
+        .post(`${api_url}/nuova-autenticazione`)
+        .send({ email: user.email, password: user.password, username: user.username });
+
+        expect(response.status).toBe(200);
+        expect(response.body.success).toBe(true);
+    });
+
+    /*
+    test('POST /api/v1/profilo/nuova-autenticazione should respond with status 500 if an error occurs', async () => {
+        const registerMock = jest.spyOn(Credenziali, 'create').mockImplementationOnce(() => {
+        throw new Error('Errore generato dal mock');
+        });
+
+        const user = {
+            email: 'test@example.com',
+            password: 'password',
+        }
+
+        const response = await request(app)
+        .post(`${api_url}/nuova-autenticazione`)
+        .send(user);
+
+        expect(response.status).toBe(500);
+        expect(response.body.success).toBe(false);
+
+        registerMock.mockRestore();
+    });
+    */
 
 
 });
